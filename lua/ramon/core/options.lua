32b656b8db8f42b98ac51843b702c778ae1284a2
RamonJales <ramonjales123@gmail.com>
2024-12-28 18:21:14 -0300

    indentation with 4 spaces

--- lua/ramon/core/options.lua
+++ lua/ramon/core/options.lua
@@ -6,8 +6,8 @@ opt.relativenumber = true
 opt.number = true
 
 --tabs and identation
-opt.tabstop = 2
-opt.shiftwidth = 2
+opt.tabstop = 4
+opt.shiftwidth = 4
 opt.expandtab = true
 opt.autoindent = true -- copy ident from current line when starting new one
 
