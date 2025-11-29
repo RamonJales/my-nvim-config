90e5a42428262236c15624c118bb663c5004f4a5
RamonJales <ramonjales123@gmail.com>
2025-11-28 21:49:16 -0300

    added todo-comments dependece, used in trouble plugin.

---
+++ lua/ramon/plugins/todo-comments.lua
@@ -0,0 +1,8 @@
+return {
+	"folke/todo-comments.nvim",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	opts = {
+		-- Aqui você pode configurar ícones, cores, etc.
+		-- Deixando vazio usa o padrão, que já é ótimo.
+	},
+}
